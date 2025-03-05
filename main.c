a1a33681be0185a4c99a189b953dd6f6bcd74d05
Eunyul <jeremia314@gmail.com>
2025-03-05 17:32:57 +0000

    added ripple carry function

--- main.c
+++ main.c
@@ -1,3 +1,18 @@
+void ripple_carry_adder(int a, int b) {
+    int sum = 0, carry = 0;
+
+    for (int i = 0; i < 4; i++) {
+        int bit_a = (a >> i) & 1;
+        int bit_b = (b >> i) & 1;
+        int bit_sum = bit_a ^ bit_b ^ carry;
+        int new_carry = (bit_a & bit_b) | (bit_b & carry) | (bit_a & carry);  // setting next bit's carry
+
+        sum |= (bit_sum << i);  // getting the sum
+        printf("Step %d:\n", i + 1);
+        printf("  Bit %d: %d + %d + %d = %d (Carry: %d)\n", i, bit_a, bit_b, carry, bit_sum, new_carry);
+        carry = new_carry;
+}
+
 int main() {
     int a, b;
     char char_a[5], char_b[5];
@@ -14,6 +29,8 @@ int main() {
         a = (a << 1) | (char_a[i] - '0');
         b = (b << 1) | (char_b[i] - '0');
     }
+
+    ripple_carry_adder(a, b);
 
     return 0;
 }
