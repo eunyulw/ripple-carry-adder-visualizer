eab109bfd7532a28b8f1b92c9c49e287bbe8df9b
Eunyul <jeremia314@gmail.com>
2025-03-05 19:08:37 +0000

    minor issue

--- display.c
+++ display.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include "adder.h"
 
-
 void binary_brackets(char* label, int num, int bits) {
     printf("%s:\n", label);
     printf("     ");
@@ -21,27 +20,4 @@ void binary_brackets(char* label, int num, int bits) {
 }
 
 void clear_screen() {
-    #include <stdio.h>
-#include "adder.h"
-
-void binary_brackets(char* label, int num, int bits) {
-    printf("%s:\n", label);
-    printf("     ");
-    for (int i = bits - 1; i >= 0; i--) {
-        printf("  ===  ");
-    }
-    printf("\n     ");
-    for (int i = bits - 1; i >= 0; i--) {
-        int bit = (num >> i) & 1;
-        printf(" | %d |", bit);
-    }
-    printf("\n     ");
-    for (int i = bits - 1; i >= 0; i--) {
-        printf("  ===  ");
-    }
-    printf("\n");
-}
-
-void clear_screen() {
-    printf("\033[H\033[J");  // clearing code i found in google
-}
+    printf("\033[H\033[J");  // clearing code
